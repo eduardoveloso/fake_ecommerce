ffde844a712d8f94fa648ba4895a3c90b6acd829
eduardoveloso <eveloso92@gmail.com>
2024-09-27 22:29:32 -0300

    fix: loading variaveis de ambiente para dentro da dag de ingestion

--- airflow/dags/ingestion.py
+++ airflow/dags/ingestion.py
@@ -9,6 +9,11 @@ project_path = (
 )
 sys.path.append(project_path)
 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+AWS_REGION = os.getenv("AWS_REGION")
+
 def ingestion_process():
     import src.ingestion_process
 
